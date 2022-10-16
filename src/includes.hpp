cf73388559df50827b741ef93433842f8eb7ab84
0xfaer <98242429+0xfaer@users.noreply.github.com>
2022-10-17 09:59:47 +1300

    Create includes.hpp

---
+++ src/includes.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <windows.h>
+#include <stdio.h>
+#include <cpr.h>
+
+#include "includes/electron/electron_framework.hpp"
+#include "includes/electron/discord_sdk/include_me.h"
+#include "includes/framework.hpp"
+
+// credits to xen0n for writing electron framework and discord sdk
